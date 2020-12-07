0ca2f0e2d54b518eee4532484c28885cc598a2c5
Geremsa <geremsa111@gmail.com>
2020-12-07 21:28:00 +0530

    animation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import Landing from './landing/landing'
+import AnimatedCursor from "react-animated-cursor"
 
 function App() {
   return (
     <main>
+   <AnimatedCursor outerSize={40}/>
       <Landing/>
     </main>
   )
--- src/landing/landing.js
+++ src/landing/landing.js
@@ -1,13 +1,16 @@
-import React from 'react'
-// import './landing.css'
+import React,{useState} from 'react'
 import './l.css'
 function Landing() {
+    const [exit, setexit] = useState(false);
     return (
-        <section className="landing">
+        <section className={`landing ${exit?"exit":""}`}>
         <img src="/images/beernn.png" alt="beer" className="hat" id="hat1"/>
         <div className="entry-text">
-        <p className="title">Happy 22 <br/> Hulk!</p>
-        <p className="enter">Let's Celebrate !</p>
+        <p className="title">
+            
+        {"Happy 22".split('').map((e,i)=><span key={i} className="letter">{e}</span>)} <br/>
+        {"Hulk !".split('').map((e,i)=><span key={i} className="letter">{e}</span>)}</p>
+        <p className="enter" onClick={()=>setexit(true)}>Let's Celebrate !</p>
         </div>
         <img src="https://www.flaticon.com/svg/static/icons/svg/609/609624.svg" alt="hat" className="balloon"/>
         </section>
